dc965c14693acc6b0012ee453d3d7bb00c585b6f
Chentse Lee <chentselee@gmail.com>
2022-08-09 01:40:25 +0800

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import netlify from "@astrojs/netlify/functions";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   output: 'server',
-  adapter: netlify()
+  adapter: process.env.ADAPTER === 'netlify' ? netlify() : cloudflare()
 });

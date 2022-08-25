6f6d2d71bbf8142437544d74c713a8587eb87b59
Chentse Lee <chentselee@gmail.com>
2022-08-26 02:32:38 +0800

    use vercel edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify/functions";
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/edge'
 
 import cloudflare from "@astrojs/cloudflare";
 

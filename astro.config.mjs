b86dd6777c9c3ddefd10c1af4ce0935a1600eb1d
Chentse Lee <chentselee@gmail.com>
2022-08-26 02:38:05 +0800

    use vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify/functions";
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel/serverless'
 
 import cloudflare from "@astrojs/cloudflare";
 

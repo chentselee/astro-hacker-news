7f7893c46d681c7745cdc9876cbc812309cfa27d
Chentse Lee <chentselee@gmail.com>
2022-08-30 04:28:04 +0800

    add home styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify/functions";
-import vercel from '@astrojs/vercel/edge'
-
+import vercel from '@astrojs/vercel/edge';
 import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), tailwind()],
   output: 'server',
   adapter: process.env.ADAPTER === 'netlify' ? netlify() : process.env.ADAPTER === 'vercel' ? vercel() : cloudflare()
 });

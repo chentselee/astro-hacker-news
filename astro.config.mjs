6d3187b5d3952b698be28fd324dab0ad4ad5438a
Chentse Lee <chentselee@gmail.com>
2022-11-26 15:12:44 +0800

    update deps. add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify/functions";
-import vercel from '@astrojs/vercel/edge';
+import vercel from "@astrojs/vercel/edge";
 import cloudflare from "@astrojs/cloudflare";
-
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
-  output: 'server',
-  adapter: process.env.ADAPTER === 'netlify' ? netlify() : process.env.ADAPTER === 'vercel' ? vercel() : cloudflare()
+  output: "server",
+  adapter:
+    process.env.ADAPTER === "netlify"
+      ? netlify()
+      : process.env.ADAPTER === "vercel"
+      ? vercel()
+      : process.env.ADAPTER === "cloudflare"
+      ? cloudflare()
+      : node({ mode: "standalone" }),
 });
